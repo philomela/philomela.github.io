5c32723e69cb12f7a27fc8a652bddb1213a0cfd1
philomela <romaphilomela@yandex.ru>
2017-10-04 06:04:32 +0300

    mag-dent-Lannding page
    Ver 1.0 Landing page on HTML+CSS

---
+++ mag-dent/js/scroll.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function() {
+  jQuery("a.scrollto").click(function () {
+    elementClick = jQuery(this).attr("href")
+    destination = jQuery(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 2100);
+    return false;
+  });
+});
