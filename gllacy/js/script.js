0814a3ec9d2f932564ea130fceaed4ca217cb2cc
philomela <romaphilomela@yandex.ru>
2017-11-08 19:16:52 +0300

    Gllacy
    Gllacy ver 1.0

---
+++ gllacy/js/script.js
@@ -0,0 +1,11 @@
+function initMap() {
+        var uluru = {lat: 55.788913, lng: 37.597336};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 17,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
